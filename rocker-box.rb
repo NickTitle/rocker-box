bb02d85b4cd3ce9200ca72617952610e7f06bb97
Nicholas Esposito <nicholasesposito@Nicholass-MacBook-Air.local>
2013-09-18 12:03:34 -0400

    added comments to rocker-box.rb

--- rocker-box.rb
+++ rocker-box.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
 
+# set the path of your static assets folder, and whether you want a flat or deep pool structure
+# flat structure will pool all html files directly inside the static folder
+# deep structure will serve index.html files inside of folders within the static folder
+
+def init	
+	enable :static
+	set :statics_folder, 'statics'
+	set :deep_or_flat_structure, 'flat'
+
+	set :page_pool, page_pool()
+end
+
+#page pool checks your pool location and returns either files or folders, depending on whether you are deep or flat
 def page_pool
 	d = Dir.entries(settings.statics_folder.to_s)
 	d_filt = Array.new
@@ -19,14 +32,8 @@ def page_pool
 	return d_filt
 end
 
-def init	
-	enable :static
-	set :statics_folder, 'statics'
-	set :deep_or_flat_structure, 'deep'
-
-	set :page_pool, page_pool()
-end
-
+#this method chooses a random page from the pool which you defined earlier
+#if the app is using deep structure, it chooses a folder, and then serves the enclosed index.html file
 def pick_random_page_from_pool
 	page_picker =  rand(settings.page_pool.count)
 	rand_page = settings.page_pool[page_picker]
@@ -43,6 +50,7 @@ end
 init()
 
 get '/' do
+	#a random page is served from your pool on each hit to the root url
 	send_file(pick_random_page_from_pool())
 end
 
